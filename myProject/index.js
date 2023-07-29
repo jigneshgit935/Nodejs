95ec52bb2e4e251885b96c5afda518cf3259a67a
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-29 12:52:11 +0530

    node-js-code-done

--- myProject/index.js
+++ myProject/index.js
@@ -3,7 +3,22 @@ const http = require('http');
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  if (req.url == '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Home</h1>');
+    res.end();
+  } else if (req.url == '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>About</h1>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>404 NOT FOUND</h1>');
+    res.end();
+  }
 });
 
 server.listen(port, () => {
